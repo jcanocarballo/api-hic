dc63401faa50353dcf1d567357dedbec961bbcfa
Julian Cano <jcanoc@frd.org.mx>
2020-10-25 02:26:42 -0600

    ADD: Se agregan metodos en cotroller de usuario para subir imagen y mostrar la imagen

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,4 +1,5 @@
 const BaseService = require('./base.service');
+const { ErrorHelper } = require('../helpers');
 
 let _userRepository = null;
 
@@ -10,6 +11,26 @@ class UserService extends BaseService {
 
     async getUserByUsername(username) {
         return await _userRepository.getUserByUsername(username);
+    }
+
+    async uploadImage(id, files){
+      if (!id) {
+        ErrorHelper.error(400, "El id no ha sido enviado.");
+      }
+      if(!files.image){
+        ErrorHelper.error(404, "No se ha subido ningun archivo.");        
+      }
+      let file_path = files.image.path;
+      let file_split = file_path.split('\\');
+      let file_name = file_split[2];
+      let ext_split = file_name.split('\.');
+      let file_ext = ext_split[1];
+
+      if(file_ext == 'png' || file_ext == 'jpg' || file_ext == 'jpeg' || file_ext == 'gif'){
+        return await this.update(id, {image: file_name})        
+      }else{
+        ErrorHelper.error(404, "Extencion no valida.");
+      }
     }
 }
 
