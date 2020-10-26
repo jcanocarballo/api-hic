54dc2f45a002b838a4ff4df5afa47b88f1495e1b
Julian Cano <jcanoc@frd.org.mx>
2020-10-25 22:04:25 -0600

    MOD: Se agregan logs en servicio para subir imagenes.

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -14,23 +14,28 @@ class UserService extends BaseService {
     }
 
     async uploadImage(id, files){
-      if (!id) {
-        ErrorHelper.error(400, "El id no ha sido enviado.");
-      }
-      if(!files.image){
-        ErrorHelper.error(404, "No se ha subido ningun archivo.");        
-      }
-      let file_path = files.image.path;
-      let file_split = file_path.split('\\');
-      let file_name = file_split[2];
-      let ext_split = file_name.split('\.');
-      let file_ext = ext_split[1];
-
-      if(file_ext == 'png' || file_ext == 'jpg' || file_ext == 'jpeg' || file_ext == 'gif'){
-        return await this.update(id, {image: file_name})        
-      }else{
-        ErrorHelper.error(404, "Extencion no valida.");
-      }
+      try {
+        if (!id) {
+          ErrorHelper.error(400, "El id no ha sido enviado.");
+        }
+        if(!files.image){
+          ErrorHelper.error(404, "No se ha subido ningun archivo.");        
+        }
+        let file_path = files.image.path;
+        console.log(file_path);
+        let file_split = file_path.split('\\');
+        let file_name = file_split[2];
+        let ext_split = file_name.split('\.');
+        let file_ext = ext_split[1];
+  
+        if(file_ext == 'png' || file_ext == 'jpg' || file_ext == 'jpeg' || file_ext == 'gif'){
+          return await this.update(id, {image: file_name})        
+        }else{
+          ErrorHelper.error(404, "Extencion no valida.");
+        }
+      } catch (error) {
+        console.log(error);        
+      }      
     }
 }
 
